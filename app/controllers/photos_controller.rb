6114f5e781a66abf96637dc5b83225e3751d3598
Jesse Delan <jesse.delan@users.noreply.github.com>
2017-04-25 11:34:07 -0500

    Finished Update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,11 +7,10 @@ class PhotosController < ApplicationController
   def show
 
     id_number = params["id_number"]
-    p = Photo.find(id_number)
-
-    @the_caption = p.caption
-    @the_image_url = p.source
-    @created_at = p.created_at
+    @photo = Photo.find(id_number)
+    @the_caption = @photo.caption
+    @the_image_url = @photo.source
+    @created_at = @photo.created_at
 
     render("/photos/show.html.erb")
   end
@@ -29,6 +28,16 @@ class PhotosController < ApplicationController
   end
 
   def edit_form
+    @the_photo_to_be_edited = Photo.find(params[:the_id])
     render("/photos/edit_form.html.erb")
   end
+
+  def update_row
+    p = Photo.find(params[:the_id])
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+    redirect_to("/photos")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get("/photos", {:controller => "photos", :action => "index"})
 
   #UPDATE
+  get("/photos/update_photo/:the_id", {:controller => "photos", :action => "update_row"})
   get("/photos/:the_id/edit", {:controller => "photos", :action => "edit_form"})
-
   #DELETE
 
 end
